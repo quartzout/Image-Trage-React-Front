6433bd69b1784f9d16cb4ccd032b34e2244a2cbc
quartzout <romatoid@gmail.com>
2023-02-15 18:29:27 +0300

    добавил компонент кнопки разлогина

---
+++ src/Components/LogoutLink.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Navigate } from "react-router-dom";
+import useAuth from "../Auth/useAuth";
+
+
+export default function LogoutLink() {
+
+    const { removeUser } = useAuth();
+
+    async function logout() {
+        const response = await fetch(process.env.REACT_APP_API_HOST + "/api/account/logout/", {
+            method: "POST",
+            credentials: "include"
+        })
+    
+        if (response.ok) {
+            removeUser()
+            Navigate("/")
+        }
+    }
+
+    return <button className="nav-item" onClick={logout}>Logout</button>
+        
+
+}
